18d8d9c709357086e7916a6b0e3b318d5c2daeab
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-03-01 15:50:44 +1100

    routing set up

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+global.Vocab = require("./api/models/vocabModels");
+const routes = require("./api/routes/vocabRoutes");
+
+mongoose.Promise = global.Promise;
+mongoose.set("useFindAndModify", false);
+mongoose.connect(
+  "mongodb+srv://theBlade:chicken420@cluster0.i0cbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { userNewUrlParser: true }
+);
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Probably replaced with: app.use(express.json());
+
+routes(app);
+app.listen(port);
+
+//default 404
+app.use((req, res) => {
+  res.status(404).send({ url: `${req.originalUrl} not found` });
+});
+console.log("Server started on http://localhost:" + port);
